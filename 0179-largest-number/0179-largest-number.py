a72c54148798f8b3f415dc17b899a5e34e7d6743
vijay sarathy <vijaysarathy1420@gmail.com>
2026-07-22 08:53:56 +0530

    Time: 3 ms (64.49%), Space: 19.2 MB (62.79%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        from functools import cmp_to_key
+        nums=list(map(str,nums))
+        nums.sort(key=cmp_to_key(lambda a,b:-1 if a+b>b+a else 1))
+        return "0" if nums[0]=="0" else "".join(nums)
